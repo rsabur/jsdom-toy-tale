a020c7f52109366f6086d32989f59398161cff86
Jabair <jabairk@gmail.com>
2021-04-12 12:02:01 -0400

    Started on Form, rendered all cards

--- src/index.js
+++ src/index.js
@@ -30,11 +30,55 @@ function renderOneToy(toyObj) {
   outerDiv.classList.add('card')
   outerDiv.dataset.id = toyObj.id
 
-  // outerDiv.html = `
-  // <div class = `
+  outerDiv.innerHTML = `
+  
+  <h2>${toyObj.name}</h2>
+  <img src=${toyObj.image} class="toy-avatar" />
+  <p>${toyObj.likes} Likes </p>
+  <button class="like-btn">Like <3</button>
+`
+
+
+
   const collectionDiv = document.querySelector('div#toy-collection')
   collectionDiv.append(outerDiv)
 }
 
+
+
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  const newToyObj = {
+    name: event.target.name.value,
+    image: event.target.url.value,
+    likes: 0
+
+
+  }
+
+  fetch("http://localhost:3000/toys", {
+    method: 'POST',
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+
+    body: JSON.stringify(newToyObj)
+  })
+    .then(resp => resp.json())
+    .then(newToyObj => {
+      renderOneToy(newToyObj)
+    })
+  form.reset()
+})
+
+
+
+
+
+renderAllToys()
 
 // id, name, img, likes
